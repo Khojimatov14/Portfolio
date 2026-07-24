139adac274e260a5e89d949c565e06ec37be585b
khojimatov <khojimatov14@gmail.com>
2026-07-24 18:20:41 +0500

    Commit

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,15 @@ URL configuration for config project.
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.urls import re_path
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(
+        url=staticfiles_storage.url('portfolio/favicon.png'),
+        permanent=True
+    )),
 ]
